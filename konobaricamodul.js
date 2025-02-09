12741e38e80160c8f713e2e8bae34d09fb2455b0
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-02-09 10:38:07 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -1,33 +1,32 @@
 module.exports = (io) => {
-   let chatContainerState = { x: 0, y: 0, width: 900, height: 600 };
-   let currentVersion = null;
-   
-    io.on('connection', (socket) => {
+    let chatContainerState = { x: 0, y: 0, width: 900, height: 600 };
+  let restoreImages ;
+  
+       io.on('connection', (socket) => {
         console.log('A user connected: ' + socket.id);
 
- if (currentVersion) {
-      socket.emit('versionLoaded', currentVersion);
-    }
-// Kada neko učita verziju, ažuriraj globalnu verziju i obavesti sve
-    socket.on('versionLoaded', (data) => {
-        currentVersion = data; // Ažuriraj poslednju verziju
-        io.emit('versionLoaded', data); // Pošalji svim povezanim korisnicima
+    socket.emit('updateChatContainer', { ...chatContainerState });
+
+// Kada klijent pošalje 'restoreImages', server emituje podatke svim povezanim korisnicima
+    socket.on('restoreImages', (images) => {
+        console.log('Received images:', images);  // Za debugging, vidiš koje slike su primljene
+        socket.broadcast.emit('restoreImages', images);  // Emituj svim drugim korisnicima
     });
 
-  socket.emit('updateChatContainer', { ...chatContainerState });
 
- socket.on('new_guest', () => {
-            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite nam raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
-                                    a naši DJ-evi će se pobrinuti da vam ispune muzičke želje. Registrovanje , Logovanje , Biranje boje , Muzika i sve ostalo 
-    sto vam je potrebno mozete naci na tabli koja se otvara klikom na dugme  G `;
-            io.emit('message', { 
-                username: '<span class="konabarica">Konobarica</span>', 
+    // Osluškuje novog gosta i šalje mu poruku dobrodošlice
+        socket.on('new_guest', () => {
+            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće i budite nam raspoloženi! 
+                                    Sada će vam vaša Konobarica poslužiti kaficu ☕, a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
+            io.emit('message', {
+                username: '<span class="konobarica">Konobarica</span>',
                 color: 'orange',
                 message: greetingMessage,
-                isSystemMessage: true 
+                isSystemMessage: true
             });
-            socket.emit('updateChatContainer', { ...chatContainerState });
-        });
+        
+ });
+        socket.emit('updateChatContainer', { ...chatContainerState });
 
         socket.on('moveChatContainer', (data) => {
             if (typeof data.x === 'number' && typeof data.y === 'number') {
@@ -37,19 +36,19 @@ module.exports = (io) => {
             }
         });
 
-      socket.on('resizeChatContainer', (data) => {
-    if (typeof data.width === 'number' && typeof data.height === 'number' && data.width > 50 && data.height > 50) {
-        chatContainerState.width = data.width;
-        chatContainerState.height = data.height;
-        io.emit('updateChatContainer', { ...chatContainerState });
-    }
-});
+        socket.on('resizeChatContainer', (data) => {
+            if (typeof data.width === 'number' && typeof data.height === 'number' && data.width > 50 && data.height > 50) {
+                chatContainerState.width = data.width;
+                chatContainerState.height = data.height;
+                io.emit('updateChatContainer', { ...chatContainerState });
+            }
+        });
 
-// Emisija za ažuriranje odmah nakon konekcije, pošto možda želiš da odmah pošalješ trenutnu veličinu
-socket.emit('updateChatContainer', { ...chatContainerState });
+        // Emisija za ažuriranje odmah nakon konekcije, pošto možda želiš da odmah pošalješ trenutnu veličinu
+        socket.emit('updateChatContainer', { ...chatContainerState });
 
-socket.on('disconnect', () => {
-    console.log('User disconnected: ' + socket.id);
-       });
+        socket.on('disconnect', () => {
+            console.log('User disconnected: ' + socket.id);
+        });
     });
 };
