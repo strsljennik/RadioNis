5b17db90dcf6137300d5500df50b7378fa8d8f0f
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-02-11 17:44:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pingService = require('./ping');
 const privatmodul = require('./privatmodul'); // Podesi putanju ako je u drugom folderu
 require('dotenv').config();
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
+
 
 
 const app = express();
@@ -35,12 +35,6 @@ app.use(express.static(__dirname + '/public'));
 app.use('/guests', uuidRouter); // Dodavanje ruta u aplikaciju
 app.set('trust proxy', true);
 app.use(cors());
-app.use(cookieParser());
-app.use((req, res, next) => {
-    res.cookie('session', 'value', { samesite: 'Strict', httpOnly: true, secure: true });
-    next();
-});
-
 
 // Rute za registraciju i prijavu
 app.post('/register', (req, res) => register(req, res, io));
