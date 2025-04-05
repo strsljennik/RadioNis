690ad4ec8c018ff706777bbb85e1a7254448233b
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-04-05 14:25:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -79,24 +79,36 @@ const ipAddress = ipList ? ipList.split(',')[0].trim() : socket.handshake.addres
     }
 
  // Emitovanje događaja da bi ostali korisnici videli novog gosta
- socket.broadcast.emit('newGuest', nickname);
+    socket.broadcast.emit('newGuest', nickname);
 io.emit('updateGuestList', Object.values(guests));
-io.emit('logMessage', `${guests[socket.id]} se povezao. IP adresa: ${ipAddress}`);
+ console.log(`${guests[socket.id]} se povezao. IP adresa korisnika: ${ipAddress}`);
+ socket.emit('new-log', `${guests[socket.id]} se povezao. IP adresa korisnika: ${ipAddress}`);
 
+ // Obrada prijave korisnika
 socket.on('userLoggedIn', (username) => {
-    io.emit('logMessage', `${guests[socket.id]} je ${username}. IP adresa: ${ipAddress}`);
-    
+    const oldNickname = guests[socket.id]; // Sačuvamo trenutni nadimak
+
+    console.log(`${oldNickname} je sada ${username}.`);
+    socket.emit('new-log', `${oldNickname} je sada ${username}.`);
+
+    guests[socket.id] = username;
+
     if (authorizedUsers.has(username)) {
-        // Ovdje možeš dodati specifične akcije za autorizovane korisnike, ako su potrebne
+        console.log(`${username} je autentifikovan kao admin.`);
+        socket.emit('new-log', `${username} je autentifikovan kao admin.`);
+    } else {
+        console.log(`${username} se prijavio kao gost.`);
+        socket.emit('new-log', `${username} se prijavio kao gost.`);
     }
-     guests[socket.id] = username;
+
     io.emit('updateGuestList', Object.values(guests));
 });
 
+
  // Obrada slanja chat poruka
     socket.on('chatMessage', (msgData) => {
-        const time = new Date().toLocaleTimeString();
-        const messageToSend = {
+     const time = new Date().toLocaleTimeString('en-GB', { timeZone: 'Europe/Berlin' });
+      const messageToSend = {
             text: msgData.text,
             bold: msgData.bold,
             italic: msgData.italic,
