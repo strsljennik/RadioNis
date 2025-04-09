adf67c5b929ac0646d7616e6b885a50f65d66dd8
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-04-09 02:35:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -85,24 +85,16 @@ io.emit('updateGuestList', Object.values(guests));
  socket.emit('new-log', `${guests[socket.id]} se povezao. IP adresa korisnika: ${ipAddress}`);
 
  // Obrada prijave korisnika
-socket.on('userLoggedIn', (username) => {
-    const oldNickname = guests[socket.id]; // Sačuvamo trenutni nadimak
-
-    console.log(`${oldNickname} je sada ${username}.`);
-    socket.emit('new-log', `${oldNickname} je sada ${username}.`);
-
-    guests[socket.id] = username;
-
-    if (authorizedUsers.has(username)) {
-        console.log(`${username} je autentifikovan kao admin.`);
-        socket.emit('new-log', `${username} je autentifikovan kao admin.`);
-    } else {
-        console.log(`${username} se prijavio kao gost.`);
-        socket.emit('new-log', `${username} se prijavio kao gost.`);
-    }
-
-    io.emit('updateGuestList', Object.values(guests));
-});
+    socket.on('userLoggedIn', (username) => {
+        if (authorizedUsers.has(username)) {
+            guests[socket.id] = username;
+            console.log(`${username} je autentifikovan kao admin.`);
+        } else {
+            guests[socket.id] = username;
+            console.log(`${username} se prijavio kao gost.`);
+        }
+        io.emit('updateGuestList', Object.values(guests));
+    });
         
  // Obrada slanja chat poruka
     socket.on('chatMessage', (msgData) => {
